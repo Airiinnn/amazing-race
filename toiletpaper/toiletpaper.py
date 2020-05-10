66177bb1618484f66b1bc9e056f7494b01b5e5f7
ningmeng618 <gu.boyuan@dhs.sg>
2020-05-10 20:04:09 +0800

    added q1

---
+++ toiletpaper/toiletpaper.py
@@ -0,0 +1,14 @@
+import sys
+sys.modules['os']=None
+sys.modules['sqlite3']=None
+sys.modules['flask']=None
+sys.modules['subprocess']=None
+sys.modules['sys']=None
+del sys
+c = 1
+s = 0
+for i in range(29):
+    s += c*2
+    c*=2
+s *=5.2
+print(int(s))
