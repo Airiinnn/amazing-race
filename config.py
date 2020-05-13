325fab1416139438853bb505dc3232b8ef8e1838
Airiinnn <alex.liswandy@hotmail.com>
2020-05-13 15:41:06 +0800

    test

--- config.py
+++ config.py
@@ -4,8 +4,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     #SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     
-
+    
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'database.db')
-
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
