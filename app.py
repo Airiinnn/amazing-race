eb346937c6b44129dabd2d498387d69f956f5ec5
Airiinnn <alex.liswandy@hotmail.com>
2020-05-14 21:04:28 +0800

    Finalised db

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ class Stage7(db.Model):
 
 
 
-'''
+
 with app.app_context():
     db.create_all()
 
@@ -170,7 +170,7 @@ for player in players:
     p = Progress(email=player[2], name=player[1], psw=player[3], group=int(player[0]))
     db.session.add(p)
     db.session.commit()
-'''
+
 
 
 
